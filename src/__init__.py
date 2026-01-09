78a4427cca8c78e307abd6136d501cd849393434
Maulana Malik Nashrulloh <mmnashrullah@gmail.com>
2026-01-10 01:47:41 +0700

    Delete src directory

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-# __init__.py
-"""
-GTAXOPROP - A utility to generate input files for taxonomy propagation and assignment in QIIME/QIIME2 from the NCBI database
-"""
-
-from .gtaxoprop import main
-
-__version__ = "1.0.0"
-__author__ = "Maulana Malik Nashrulloh, Sonia Az Zahra Defi, Brian Rahardi, Muhammad Badrut Tamam, Riki Ruhimat, Hessy Novita"
